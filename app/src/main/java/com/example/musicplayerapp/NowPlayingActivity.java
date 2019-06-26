88b006aff729f1727d0597d502ece7b61591df6f
mwarne97 <m.warner976@hotmail.com>
2019-06-26 15:20:15 -0400

    Included app in use gif

--- app/src/main/java/com/example/musicplayerapp/NowPlayingActivity.java
+++ app/src/main/java/com/example/musicplayerapp/NowPlayingActivity.java
@@ -2,6 +2,7 @@ package com.example.musicplayerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
